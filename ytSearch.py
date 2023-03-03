bfc1c95122085d4b0389f5c79a1477c2cc6defe2
sleddy12 <scottledyard2024@u.northwestern.edu>
2023-03-03 17:37:38 -0600

    added google search, some question answering

--- ytSearch.py
+++ ytSearch.py
@@ -1,4 +1,6 @@
 from youtube_search import YoutubeSearch
+import requests
+from bs4 import BeautifulSoup
 
 #returns link for first result of searched input
 def search_youtube(searchStr: str) -> str:
@@ -6,6 +8,15 @@ def search_youtube(searchStr: str) -> str:
     link = "https://youtube.com"+results[0]['url_suffix']
     return link
 
-
+#returns link for first result of searched input
+def search_google(query: str) -> str:
+    query = query.replace(" ", "+")
+    url = f"https://www.google.com/search?q={query}&num=1"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "lxml")
+    link = soup.find('div', {"class": "egMi0 kCrYT"}).a['href']
+    link = link.split("&")[0]
+    link = link.split("=")[1]
+    return link
 
 
